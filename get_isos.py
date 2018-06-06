a2d460972febe4fa80893ac141a21849c3b2e301
f-ardila <fardila@ucsc.edu>
2018-06-06 13:54:58 -0700

    looked at new highres maps from Benedikt and plotted new profiles matched by m100 and m_halo. also compared pixel effects to quick maps

--- get_isos.py
+++ get_isos.py
@@ -6,57 +6,91 @@ from functions import *
 import time
 time0=time.time()
 
-#data
-Illustris_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/galaxies_stellarmaps_orig_11.2.hdf5'
-TNG_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_stellarmaps_tng75_11.2.hdf5'
+import sys
+maps=sys.argv[1]
 
-#which components?
-comp = 'cen+icl'
-###############################################################################
-#run on Illustris
-isos_illustris=[]
+if maps=='quick':
 
-for i in range(339):
+    #data
+    Illustris_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/galaxies_stellarmaps_orig_11.2.hdf5'
+    TNG_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_stellarmaps_tng75_11.2.hdf5'
+
+    #which components?
+    comp = 'cen+icl'
+    ###############################################################################
+    #run on Illustris
+    isos_illustris=[]
+
+    for i in range(339):
 
 
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-    print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
-    try:
-        iso= get_iso(Illustris_file_quick,'Illustris', components=comp, gal_n=i)
+        try:
+            iso= get_iso(Illustris_file_quick,'Illustris', components=comp, gal_n=i)
 
-    except:
-        iso=-99.99
+        except:
+            iso=-99.99
 
-    isos_illustris.append(iso)
+        isos_illustris.append(iso)
 
-#save as pickles
-pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_isos_quick_'+ comp +'.pkl','wb')
-pickle.dump(isos_illustris,pkl_isos)
-pkl_isos.close()
+    #save as pickles
+    pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_isos_quick_'+ comp +'.pkl','wb')
+    pickle.dump(isos_illustris,pkl_isos)
+    pkl_isos.close()
 
-###############################################################################
-#run on TNG
-isos_tng=[]
+    ###############################################################################
+    #run on TNG
+    isos_tng=[]
 
-for i in range(235):
+    for i in range(235):
 
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-    print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
-    try:
-        iso= get_iso(TNG_file_quick,'TNG', components=comp, gal_n=i)
+        try:
+            iso= get_iso(TNG_file_quick,'TNG', components=comp, gal_n=i)
 
-    except:
-        iso=-99.99
+        except:
+            iso=-99.99
 
-    isos_tng.append(iso)
+        isos_tng.append(iso)
 
-#save as pickles
-pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_quick_'+ comp +'.pkl','wb')
-pickle.dump(isos_tng,pkl_isos)
-pkl_isos.close()
+    #save as pickles
+    pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_quick_'+ comp +'.pkl','wb')
+    pickle.dump(isos_tng,pkl_isos)
+    pkl_isos.close()
+
+elif maps=='highres':
+    TNG_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_stellarmaps_tng75_11.2_highres.hdf5'
+
+    #which components?
+    comp = 'cen'
+    ###############################################################################
+    #run on TNG
+    isos_tng=[]
+
+    for i in range(235):
+
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
+        try:
+            iso= get_iso(TNG_file_quick,'TNG_highres', components=comp, gal_n=i)
+
+        except:
+            iso=-99.99
+
+        isos_tng.append(iso)
+
+    #save as pickles
+    pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_highres_'+ comp +'.pkl','wb')
+    pickle.dump(isos_tng,pkl_isos)
+    pkl_isos.close()
+
 
 print(time.time()-time0, ' seconds')
--- get_masses.py
+++ get_masses.py
@@ -6,6 +6,9 @@ from functions import *
 import time
 time0=time.time()
 
+import sys
+maps=sys.argv[1]
+
 #data
 Illustris_file = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/galaxies_orig_11.2.hdf5'
 TNG_file = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_tng75_11.2.hdf5'
@@ -13,67 +16,104 @@ TNG_file = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_tng75
 Illustris_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/galaxies_stellarmaps_orig_11.2.hdf5'
 TNG_file_quick = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_stellarmaps_tng75_11.2.hdf5'
 
-###############################################################################
-#run on Illustris
-isos_illustris=[]
-masses_illustris=[]
-
-for i in range(339):  
-
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-    print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-
-    try:
-        iso, masses = get_masses(Illustris_file_quick,'Illustris',gal_n=i)
-
-    except ValueError:
-        iso=-99.99
-        masses=-99.99
+TNG_file_highres = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/galaxies_stellarmaps_tng75_11.2_highres.hdf5'
 
 
-    isos_illustris.append(iso)
-    masses_illustris.append(masses)
+if maps=='quick':
+    ###############################################################################
+    #run on Illustris
+    isos_illustris=[]
+    masses_illustris=[]
 
-#save as pickles
-pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_isos_quick.pkl','wb')
-pickle.dump(isos_illustris,pkl_isos)
-pkl_isos.close()
+    for i in range(339):
 
-pkl_masses = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_masses_quick.pkl','wb')
-pickle.dump(masses_illustris,pkl_masses)
-pkl_masses.close()
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
-###############################################################################
-#run on TNG
-isos_tng=[]
-masses_tng=[]
+        try:
+            iso, masses = get_masses(Illustris_file_quick,'Illustris',gal_n=i)
 
-for i in range(235):
-
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-    print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-
-    try:
-        iso, masses = get_masses(TNG_file_quick,'TNG',gal_n=i)
-
-    except:
-        iso=-99.99
-        masses=-99.99
+        except ValueError:
+            iso=-99.99
+            masses=-99.99
 
 
+        isos_illustris.append(iso)
+        masses_illustris.append(masses)
 
-    isos_tng.append(iso)
-    masses_tng.append(masses)
+    #save as pickles
+    pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_isos_quick.pkl','wb')
+    pickle.dump(isos_illustris,pkl_isos)
+    pkl_isos.close()
 
-#save as pickles
-pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_quick.pkl','wb')
-pickle.dump(isos_tng,pkl_isos)
-pkl_isos.close()
+    pkl_masses = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/Illustris/Illustris_masses_quick.pkl','wb')
+    pickle.dump(masses_illustris,pkl_masses)
+    pkl_masses.close()
 
-pkl_masses = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_masses_quick.pkl','wb')
-pickle.dump(masses_tng,pkl_masses)
-pkl_masses.close()
+    ###############################################################################
+    #run on TNG
+    isos_tng=[]
+    masses_tng=[]
+
+    for i in range(235):
+
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
+        try:
+            iso, masses = get_masses(TNG_file_quick,'TNG',gal_n=i)
+
+        except:
+            iso=-99.99
+            masses=-99.99
+
+
+
+        isos_tng.append(iso)
+        masses_tng.append(masses)
+
+    #save as pickles
+    pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_quick.pkl','wb')
+    pickle.dump(isos_tng,pkl_isos)
+    pkl_isos.close()
+
+    pkl_masses = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_masses_quick.pkl','wb')
+    pickle.dump(masses_tng,pkl_masses)
+    pkl_masses.close()
+
+elif maps == 'highres':
+        ###############################################################################
+        #run on TNG
+        isos_tng=[]
+        masses_tng=[]
+
+        for i in range(235):
+
+            print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+            print('^^^^^^^^GALAXY '+str(i)+'^^^^^^^^^^^^^^')
+            print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
+            try:
+                iso, masses = get_masses(TNG_file_highres,'TNG_highres',gal_n=i)
+
+            except:
+                iso=-99.99
+                masses=-99.99
+
+
+
+            isos_tng.append(iso)
+            masses_tng.append(masses)
+
+        #save as pickles
+        pkl_isos = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_isos_highres.pkl','wb')
+        pickle.dump(isos_tng,pkl_isos)
+        pkl_isos.close()
+
+        pkl_masses = open('/Users/fardila/Documents/GitHub/HSC_vs_hydro/Data/TNG/TNG_masses_highres.pkl','wb')
+        pickle.dump(masses_tng,pkl_masses)
+        pkl_masses.close()
 
 print(time.time()-time0, ' seconds')
