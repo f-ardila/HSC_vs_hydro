7d27688b020014f6ac4d9c36323f9783f660b692
Felipe Ardila <fardila@dhcp-67-95.ucsc.edu>
2019-08-06 11:48:12 -0700

    added correct map file (clusterfinder one). got masses directly from get_1d_masses() rather than trying to get all masses. some filesgalaxies would give an error for 2D masses. we only have low res file.

--- CC_isos.py
+++ CC_isos.py
@@ -21,8 +21,10 @@ time0=time.time()
 components = 'cen'
 
 xray_file = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/xray_clusterfinder_tng300_100.hdf5'
-maps_file_highres = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/galaxies_stellarmaps_hr_11.2_tng300_072.hdf5'
-maps_file_quick= '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/galaxies_stellarmaps_lr_11.2_tng300_072.hdf5'
+# maps_file_highres = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/galaxies_stellarmaps_hr_11.2_tng300_072.hdf5'
+# maps_file_quick= '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/galaxies_stellarmaps_lr_11.2_tng300_072.hdf5'
+maps_file_quick= '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/galaxies_clusterfinder_lr_tng300_099.hdf5'
+
 
 
 f = h5py.File(xray_file, 'r')
@@ -30,10 +32,10 @@ xray_ids = np.array(f['catgrp_id'])
 f.close()
 
 
-f = h5py.File(maps_file_highres, 'r')
+f = h5py.File(maps_file_quick, 'r')
 all_map_indexes = range(len(np.array(f['catgrp_is_primary'])))
 mask = np.in1d(np.array(f['catgrp_id']), xray_ids) & np.array(f['catgrp_is_primary'])
-maps_indexes_to_use = np.array(all_map_indexes)[mask]
+maps_indexes_to_use = np.array(all_map_indexes)[mask][:]
 
 print(maps_indexes_to_use)
 f.close()
@@ -44,24 +46,24 @@ f.close()
 resolution='quick'
 isos_tng = [get_iso(maps_file_quick, 'TNG300', resolution, intMode='mean',
                     components=components, gal_n=i) for i in maps_indexes_to_use]
-masses_tng = [get_masses(isos_tng[i], TNG_file, 'TNG', resolution,
-                         rs=[300, 500, 800], gal_n=i) for i in maps_indexes_to_use]
+oneD_masses_tng = np.array([get_1d_masses(iso) for iso in isos_tng])
 
 # save as pickles
 outfile_loc = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/'
 save_pkl(outfile_loc+'CC_TNG300_isos_{0}_{1}.pkl'.format(components, resolution), isos_tng)
 save_pkl(outfile_loc+'CC_TNG300_masses_{0}_{1}.pkl'.format(components,
-                                                     resolution), vstack(masses_tng))
-                                                     
+                                                     resolution), np.concatenate(oneD_masses_tng))
+
 ###############################################################################
-resolutions = 'highres'
-isos_tng = [get_iso(maps_file_highres, 'TNG300', resolution, intMode='mean',
-                    components=components, gal_n=i) for i in maps_indexes_to_use]
-masses_tng = [get_masses(isos_tng[i], TNG_file, 'TNG', resolution,
-                         rs=[300, 500, 800], gal_n=i) for i in maps_indexes_to_use]
-
-# save as pickles
-outfile_loc = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/'
-save_pkl(outfile_loc+'CC_TNG300_isos_{0}_{1}.pkl'.format(components, resolution), isos_tng)
-save_pkl(outfile_loc+'CC_TNG300_masses_{0}_{1}.pkl'.format(components,
-                                                     resolution), vstack(masses_tng))
+#we dont thave the highres file
+# resolution = 'highres'
+# isos_tng = [get_iso(maps_file_highres, 'TNG300', resolution, intMode='mean',
+#                     components=components, gal_n=i) for i in maps_indexes_to_use]
+# oneD_masses_tng = [get_1d_masses(iso) for iso in isos_tng]
+#
+#
+# # save as pickles
+# outfile_loc = '/Users/fardila/Documents/GitHub/HSC_vs_hydro/CC/'
+# save_pkl(outfile_loc+'CC_TNG300_isos_{0}_{1}.pkl'.format(components, resolution), isos_tng)
+# save_pkl(outfile_loc+'CC_TNG300_masses_{0}_{1}.pkl'.format(components,
+#                                                      resolution), vstack(oneD_masses_tng))
